5d83c4639c43913f1d10a6d62d3527bafaa84b10
kanweitech <68553986+kanweitech@users.noreply.github.com>
2025-10-18 13:39:21 +0100

    Fix indentation in User model definition

--- app/models.py
+++ app/models.py
@@ -3,8 +3,8 @@ from app.db.base import Base
 
 
 class User(Base):
-  __tablename__ = "users"
-  id = Column(Integer, primary_key=True, index=True)
-  email = Column(String, unique=True, index=True, nullable=False)
-  full_name = Column(String, nullable=True)
-  password = Column(String, nullable=False)
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    full_name = Column(String, nullable=True)
+    password = Column(String, nullable=False)
