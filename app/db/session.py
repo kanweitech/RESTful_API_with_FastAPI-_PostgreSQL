7e5ef9d7aea5d774fb7814a4d9aee59bdb69a5b8
kanweitech <68553986+kanweitech@users.noreply.github.com>
2025-10-18 13:30:12 +0100

    Add SQLAlchemy session management setup

---
+++ app/db/session.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import os
+
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/postgres")
+
+
+engine = create_engine(DATABASE_URL, future=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
